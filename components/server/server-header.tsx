177478327b8d9915090fa049dcea6558d0f2e609
Rajdeep Gill <35fear35@gmail.com>
2023-12-31 11:17:50 -0600

    Create new channels inside servers

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -71,6 +71,7 @@ export const ServerHeader = ({
                 {isModerator && (
                     <DropdownMenuItem
                         className =  "px-3 py-2 text-sm cursor-pointer"
+                        onClick = {() => onOpen("createChannel", { server: server })}
                     >
                         Create Channel
                         <PlusCircle className = "h-4 w-4 ml-auto"/>
