99ab1fa9f795e2fee562ef0cdb9fc9d09a455234
Rajdeep Gill <35fear35@gmail.com>
2024-01-01 12:06:05 -0600

    Added search functionality for channels and members

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,12 +1,28 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 import { ServerHeader } from "./server-header";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ServerSearch } from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Button } from "../ui/button";
 interface ServerSidebarProps{
     serverId: string;
+}
+
+const iconMap = {
+    [ChannelType.TEXT]: <Hash className = "mr-2 h-4 w-4"/>,
+    [ChannelType.AUDIO]: <Mic className = "mr-2 h-4 w-4"/>,
+    [ChannelType.VIDEO]: <Video className = "mr-2 h-4 w-4"/>
+    
+}
+
+const roleIconMap = {
+    [MemberRole.GUEST]: null,
+    [MemberRole.MODERATOR]: <ShieldCheck className = "mr-2 h-4 w-4 text-indigo-500"/>,
+    [MemberRole.ADMIN]: <ShieldAlert className = "mr-2 h-4 w-4 text-rose-500"/>
 }
 
 export const ServerSideBar = async ({
@@ -40,11 +56,11 @@ export const ServerSideBar = async ({
         }
     });
 
+    
     const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
     const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
     const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
     const members = server?.members.filter((member) => member.profileId !== profile.id);
-
     if(!server){
         return redirect("/");
     }
@@ -57,6 +73,50 @@ export const ServerSideBar = async ({
                 server = {server}
                 role = {role}
             />
+            <ScrollArea className = "flex-1 px-3">
+                <div className = "mt-2">
+                    <ServerSearch 
+                        data = {[
+                            {
+                                label: "Text Channels",
+                                type: "channel",
+                                data: textChannels?.map((channel) => ({
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                    id: channel.id
+                                }))
+                            },
+                            {
+                                label: "Voice Channels",
+                                type: "channel",
+                                data: audioChannels?.map((channel) => ({
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                    id: channel.id
+                                }))
+                            },
+                            {
+                                label: "Video Channels",
+                                type: "channel",
+                                data: videoChannels?.map((channel) => ({
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                    id: channel.id
+                                }))
+                            },
+                            {
+                                label: "Members",
+                                type: "member",
+                                data: members?.map((member) => ({
+                                    name: member.profile.name,
+                                    icon: roleIconMap[member.role],
+                                    id: member.id
+                                }))
+                            },
+                        ]}
+                    />
+                </div>
+            </ScrollArea>
         </div>
     )
 }
