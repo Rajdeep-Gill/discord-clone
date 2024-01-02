15193db8a871fb6708b4244f8f70cb97974dd8aa
Rajdeep Gill <35fear35@gmail.com>
2024-01-02 17:59:21 -0600

    Refactor SocketProvider component

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -1,44 +1,48 @@
 "use client";
 
-import {
-    createContext,
-    useContext,
-    useEffect,
-    useState,
+import { 
+  createContext,
+  useContext,
+  useEffect,
+  useState
 } from "react";
 
 import { io as ClientIO } from "socket.io-client";
 
 type SocketContextType = {
-    socket: any | null;
-    isConnected: boolean;
-}
+  socket: any | null;
+  isConnected: boolean;
+};
 
 const SocketContext = createContext<SocketContextType>({
-    socket: null,
-    isConnected: false,
+  socket: null,
+  isConnected: false,
 });
 
 export const useSocket = () => {
-    return useContext(SocketContext);
-}
+  return useContext(SocketContext);
+};
 
-export const SocketProivder = ({ children }: {children: React.ReactNode}) => {
+export const SocketProvider = ({ 
+    children 
+}: { 
+    children: React.ReactNode 
+}) => {
     const [socket, setSocket] = useState(null);
     const [isConnected, setIsConnected] = useState(false);
 
     useEffect(() => {
         const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
-            path: "/api/socket/io",
-            addTrailingSlash: false,
-        });
+        path: "/api/socket/io",
+        addTrailingSlash: false,
+    });
 
         socketInstance.on("connect", () => {
             setIsConnected(true);
         });
 
         socketInstance.on("disconnect", () => {
-            setIsConnected(false);
+    setIsConnected(false);
         });
 
         setSocket(socketInstance);
@@ -46,12 +50,11 @@ export const SocketProivder = ({ children }: {children: React.ReactNode}) => {
         return () => {
             socketInstance.disconnect();
         }
-    }, [])
+    }, []);
 
     return (
         <SocketContext.Provider value={{ socket, isConnected }}>
-            
+            {children}
         </SocketContext.Provider>
     )
-
 }
