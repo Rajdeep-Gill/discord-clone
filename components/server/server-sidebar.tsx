912886e7ea461e7564b26ac12241405d1cefd9c2
Rajdeep Gill <35fear35@gmail.com>
2024-01-02 14:53:00 -0600

    Add edit and delete channel functionality

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -120,6 +120,7 @@ export const ServerSideBar = async ({
                         ]}
                     />
                 </div>
+                <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2"/>
                 {!!textChannels?.length && (
                     <div className = "mb-2">
                         <ServerSection 
