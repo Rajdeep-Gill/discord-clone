bca068902db0fc9a4a9c8e207e63d53fd8e36556
Rajdeep Gill <35fear35@gmail.com>
2024-01-02 18:00:17 -0600

    Refactor API socket configuration

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -6,18 +6,19 @@ import { NextApiResponseServerIo } from "@/types";
 
 export const config = {
     api: {
-        bodyParser: false
-    }
+        bodyParser: false,
+    },
 };
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
-    if(!res.socket.server.io){
+    if (!res.socket.server.io) {
         const path = "/api/socket/io";
         const httpServer: NetServer = res.socket.server as any;
         const io = new ServerIO(httpServer, {
             path: path,
-            addTrailingSlash: false
+            addTrailingSlash: false,
         });
+
         res.socket.server.io = io;
     }
 
