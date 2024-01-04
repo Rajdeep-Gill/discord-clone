b93739c99cea02815d6df2860fe63c6228764536
Rajdeep Gill <35fear35@gmail.com>
2024-01-04 12:28:09 -0600

    Add Chat item component and edit message functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,12 +5,17 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { format } from "date-fns";
+import { ChatItem } from "./chat-item";
+
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
         profile: Profile
     }
 }
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 interface ChatMessagesProps {
     name: string;
     member: Member;
@@ -80,9 +85,19 @@ export const ChatMessages = ({
                 {data?.pages?.map((group, i) => (
                     <Fragment key = {i}>
                         {group.items.map((message: MessageWithMemberWithProfile ) => (
-                            <div key = {message.id}>
-                                {message.content}
-                            </div>
+                            <ChatItem
+                                key={message.id}
+                                id={message.id}
+                                currentMember={member}
+                                member={message.member}
+                                content={message.content}
+                                fileUrl={message.fileUrl}
+                                deleted={message.deleted}
+                                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                isUpdated={message.updatedAt !== message.createdAt}
+                                socketUrl={socketUrl}
+                                socketQuery={socketQuery}
+                            />
                         ))}
                     </Fragment>
                 ))}
